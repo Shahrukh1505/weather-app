9aef830cfa86ad0bfb21907cf559a41d4f5c1bc7
Shahrukh1505 <shahrukhansari15@gmail.com>
2022-08-23 19:39:17 +0530

    latest release

--- src/component/Container.js
+++ src/component/Container.js
@@ -11,6 +11,7 @@ const Container = ()=>{
    const [tempMin, setTempMin] = useState(25);
    const [tempMax, setTempMax] = useState(25);
    const [date, setDate] = useState("0000-00-00");
+   const [status, setStatus] = useState(true);
    const searchButton = (e) => {
             e.preventDefault();
             setVal(search);
@@ -26,15 +27,13 @@ const Container = ()=>{
 
             
             try{
+            
                 const url = `https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=d765d356ebd7e11900c84780738686b3`;
                 const response = await fetch(url);
 
-                
-               
+                if(response.ok){
+                    setStatus(true);
                     const data = await response.json();
-
-
-
                     console.log(data);
                     setCity(data);
                     const t = parseInt(city.main.temp)-273;
@@ -45,17 +44,32 @@ const Container = ()=>{
                     setTempMin(tMin);
                     const tMax = parseInt(city.main.temp_max)-273;
                     setTempMax(tMax); 
+                }
+                else{
+                            setStatus(false);
+                }
+               
+                  
+            
+               
+
+
+
+
+                  
 
              
             }
             catch(error){
-                setCity(null);
+            //    setVal("");
+              
+
                 console.log("Someone seriously needs to get out");
                 console.log(error);
             }
                 
              
-               
+               console.log("val " + val);
              
 
           }  
@@ -77,11 +91,11 @@ const Container = ()=>{
         <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={searchButton}>Search</button>
     </div>
 </form>
-{!city ? (<div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-  <strong class="font-bold">Holy smokes!</strong>
-  <span class="block sm:inline">The city doesn't exist</span>
-  <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+{!status ? (<div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+  <strong className="font-bold">Holy smokes!</strong>
+  <span className="block sm:inline">The city doesn't exist</span>
+  <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
   </span>
 </div>) :(
     <div className="flex flex-wrap mt-10">
